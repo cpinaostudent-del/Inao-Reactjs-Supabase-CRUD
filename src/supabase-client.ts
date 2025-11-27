47016b7e4f8749a087f61e4e0a3c3fdc706df010
unknown <COMPLAB4@COMPLAB4-12.ASIANCOLLEGE.LOCAL>
2025-11-27 08:50:35 +0800

    new-update

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -1,16 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabase_PROJECT_URL = import.meta.env.VITE_SUPABASE_URL;
-const supabase_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const SUPABASE_PROJECT_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabase_PROJECT_URL, supabase_ANON_KEY);
-
-
-
-
-
-
-
-
-
-
+export const supabase = createClient(SUPABASE_PROJECT_URL, SUPABASE_ANON_KEY);
